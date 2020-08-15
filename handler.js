3a1f9f8a2cb833a115d7bb267dc489e36fad1553
awsdashboardpricing@gmail.com <awsdashboardpricing@gmail.com>
2020-08-15 14:39:10 -0500

    send filePath to node server

--- handler.js
+++ handler.js
@@ -4,7 +4,9 @@ const axios = require('axios');
 'use strict';
 
 module.exports.sendS3Path = async event => {
-  const result = axios.get('http://localhost:3000/').then(response => {
+  const result = axios.post('http://localhost:3000/s3persist', {
+    filePath: "AWSLogs/261786166738/Config/us-east-2/2020/8/6/ConfigHistory/261786166738_Config_us-east-2_ConfigHistory_AWS::EC2::InternetGateway_20200806T183059Z_20200806T183059Z_1.json.gz"
+}).then(response => {
     console.log(response.data.message)
     return {
       statusCode: 200,
