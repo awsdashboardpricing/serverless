70208fdd76a0439358a42e1eda0e0cd15e3d3812
awsdashboardpricing@gmail.com <awsdashboardpricing@gmail.com>
2020-08-15 09:47:02 -0500

    add request routes

--- handler.js
+++ handler.js
@@ -1,18 +1,22 @@
+const https = require('https');
+
+
 'use strict';
 
 module.exports.sendS3Path = async event => {
+  const req = https.get('https://localhost:3000/')
+  req.on('data', data => {
+    console.log(data)
+  })
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    // body: JSON.stringify(
+    //   {
+    //     path: 'AWSLogs/261786166738/Config/us-east-2/2020/8/6/ConfigHistory/261786166738_Config_us-east-2_ConfigHistory_AWS::EC2::InternetGateway_20200806T183059Z_20200806T183059Z_1.json.gz'
+    //   },
+    //   null,
+    //   2
+    // ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
