f2578d96213ffb992e4daf901a77864afc83797d
awsdashboardpricing@gmail.com <awsdashboardpricing@gmail.com>
2020-08-15 10:17:46 -0500

    send request to localhost:3000

--- handler.js
+++ handler.js
@@ -1,22 +1,20 @@
-const https = require('https');
+const axios = require('axios');
 
 
 'use strict';
 
 module.exports.sendS3Path = async event => {
-  const req = https.get('https://localhost:3000/')
-  req.on('data', data => {
-    console.log(data)
+  const result = axios.get('http://localhost:3000/').then(response => {
+    console.log(response.data.message)
+    return {
+      statusCode: 200,
+      body: response.data
+    };
+  }).catch( error => {
+    return {
+      statusCode: 400
+    }
   })
 
-  return {
-    statusCode: 200,
-    // body: JSON.stringify(
-    //   {
-    //     path: 'AWSLogs/261786166738/Config/us-east-2/2020/8/6/ConfigHistory/261786166738_Config_us-east-2_ConfigHistory_AWS::EC2::InternetGateway_20200806T183059Z_20200806T183059Z_1.json.gz'
-    //   },
-    //   null,
-    //   2
-    // ),
-  };
+  return result
 };
